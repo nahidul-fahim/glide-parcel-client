5cc45f2e55cc00ec802bc71e7d91073092230c85
nahidul-fahim <nahidulfahim.official@gmail.com>
2023-11-25 00:42:02 +0600

    created axiosopen and axiossecure file

--- src/Hooks/useAxiosSecure/useAxiosSecure.jsx
+++ src/Hooks/useAxiosSecure/useAxiosSecure.jsx
@@ -1,47 +1,47 @@
-import axios from "axios";
-import { useNavigate } from "react-router-dom";
-import useAuthProvider from "../useAuthProvider/useAuthProvider";
+// import axios from "axios";
+// import { useNavigate } from "react-router-dom";
+// import useAuthProvider from "../useAuthProvider/useAuthProvider";
 
 
-const axiosSecure = axios.create({
-    baseURL: 'http://localhost:5000',
-    // withCredentials: true,
-})
+// const axiosSecure = axios.create({
+//     baseURL: 'http://localhost:5000',
+//     // withCredentials: true,
+// })
 
-const useAxiosSecure = () => {
+// const useAxiosSecure = () => {
 
-    const navigate = useNavigate();
-    const { LogOut } = useAuthProvider();
+//     const navigate = useNavigate();
+//     const { LogOut } = useAuthProvider();
 
-    // request interceptor to add authorization header
-    axiosSecure.interceptors.request.use(function (config) {
-        const token = localStorage.getItem('access-token');
-        config.headers.authorization = `Bearer ${token}`
-        return config;
-    },
-        function (error) {
-            // Do something with request error
-            return Promise.reject(error);
-        })
-
-
-    // intercepts 401 and 403 status
-    axiosSecure.interceptors.response.use(function (response) {
-        return response;
-    }, async (error) => {
-        const status = error.response.status;
-        console.log(error);
-
-        if (status === 401 || status === 403) {
-            await LogOut();
-            navigate('/login');
-        }
-        return Promise.reject(error);
-    })
+//     // request interceptor to add authorization header
+//     axiosSecure.interceptors.request.use(function (config) {
+//         const token = localStorage.getItem('access-token');
+//         config.headers.authorization = `Bearer ${token}`
+//         return config;
+//     },
+//         function (error) {
+//             // Do something with request error
+//             return Promise.reject(error);
+//         })
 
 
+//     // intercepts 401 and 403 status
+//     axiosSecure.interceptors.response.use(function (response) {
+//         return response;
+//     }, async (error) => {
+//         const status = error.response.status;
+//         console.log(error);
 
-    return axiosSecure;
-};
+//         if (status === 401 || status === 403) {
+//             await LogOut();
+//             navigate('/login');
+//         }
+//         return Promise.reject(error);
+//     })
 
-export default useAxiosSecure;
+
+
+//     return axiosSecure;
+// };
+
+// export default useAxiosSecure;
