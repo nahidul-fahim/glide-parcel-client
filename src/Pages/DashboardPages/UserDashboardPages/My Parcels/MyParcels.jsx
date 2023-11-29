b30a0ccb4cd040211aaf4b1b628e46d1e8525b56
nahidul-fahim <nahidulfahim.official@gmail.com>
2023-11-29 12:32:27 +0600

    got all the parcels by user

--- src/Pages/DashboardPages/UserDashboardPages/My Parcels/MyParcels.jsx
+++ src/Pages/DashboardPages/UserDashboardPages/My Parcels/MyParcels.jsx
@@ -1,4 +1,6 @@
 import useParcels from "../../../../Hooks/useParcels/useParcels";
+import { GrUpdate } from "react-icons/gr";
+import { GiCancel } from "react-icons/gi";
 
 
 const MyParcels = () => {
@@ -19,17 +21,65 @@ const MyParcels = () => {
 
 
 
-
-
-
-
     return (
         <div className="w-full flex flex-col justify-center items-center gap-10 py-5">
             <h2 className="text-5xl font-heading text-third font-bold">My <span className="text-main">Parcels</span></h2>
 
-            <div className="flex justify-between items-center w-full px-5">
-                <h3 className="font-body font-semibold text-darkgray text-xl">Total parcels: <span className="text-third">{parcels.length}</span></h3>
-                <h3 className="font-body font-semibold text-darkgray text-xl">Total cost: <span className="text-third">${totalCost}</span></h3>
+            {/* total parcels + total cost */}
+            <div className="flex flex-col justify-center items-start w-full gap-5 px-5">
+                <h3 className="font-body font-semibold text-darkgray text-xl">Total parcels: <span className="text-sub">{parcels.length}</span></h3>
+                <h3 className="font-body font-semibold text-darkgray text-xl">Total cost: <span className="text-sub">${totalCost}</span></h3>
+            </div>
+
+            {/* all parcels table */}
+            <div className="w-full">
+                <div className="overflow-x-auto">
+                    <table className="table">
+                        {/* head */}
+                        <thead>
+                            <tr className="font-inter">
+                                <th>#</th>
+                                <th>Parcel type</th>
+                                <th>Requested delivery date</th>
+                                <th>Approximate delivery date</th>
+                                <th>Booking date</th>
+                                <th>Deliver man</th>
+                                <th>Booking status</th>
+                                <th>Manage booking</th>
+                                <th>Review</th>
+                                <th>Payment</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+
+                            {
+                                parcels.map((parcel, index) => <tr key={parcel._id}>
+                                    <th>
+                                        {index + 1}
+                                    </th>
+                                    <td>
+                                        <h4 className="font-medium font-inter text-[14px]">{parcel.parcelType}</h4>
+                                    </td>
+                                    <td>
+                                        <h4 className="font-medium font-inter text-[14px]">{parcel.delvDate}</h4>
+                                    </td>
+                                    <td className="font-inter font-semibold text-[14px]">
+                                        <h4 className="font-medium font-inter text-[14px]">{parcel.delvDate}</h4>
+                                    </td>
+                                    <td className="font-inter font-semibold text-[14px]">
+                                        <h4 className="font-medium font-inter text-[14px]">{parcel.bookingDate}</h4>
+                                    </td>
+                                    <td className="font-inter font-semibold text-[14px]">
+                                        <h4 className="font-medium font-inter text-[14px]">X</h4>
+                                    </td>
+                                    <td className="font-inter font-semibold text-[14px]">
+                                        <h4 className="font-medium font-inter text-[14px]">Good</h4>
+                                    </td>
+                                </tr>)
+                            }
+                        </tbody>
+                    </table>
+                </div>
             </div>
         </div>
     );
